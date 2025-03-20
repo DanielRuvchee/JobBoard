1fdd29afc081f08ff4c5d9bb90f0b35a18cabf61
Daniel <daniel.ruvcheski@students.finki.ukim.mk>
2025-03-20 21:27:25 +0100

    Add GitHub sign-in action and refactor LoginForm to use GeneralSubmitButtons component

---
+++ components/general/SubmitButtons.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { useFormStatus } from "react-dom";
+import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
+import { ReactNode } from "react";
+
+interface GeneralSubmitButtonProps {
+    text: string;
+    variant?:
+    | "default"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | "destructive"
+    | null
+    | undefined
+
+    width?: string
+    icon?: ReactNode
+}
+
+export function GeneralSubmitButtons({text, variant, width, icon}: GeneralSubmitButtonProps){
+    
+        const { pending } = useFormStatus()
+
+        return <Button variant={variant} className={width}>
+            {pending ? (
+                <>
+                <Loader2 className="size-4 animate-spin" />
+                <span>Submitting...</span>
+                </>
+            ) : (
+                <>
+                {icon && <div>{icon}</div>}
+                <span>{text}</span>
+                </>
+            )}
+        </Button>
+        
+        }
