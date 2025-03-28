465fa3cfbe2835922ac25b7ae7cb1818c0768d77
Daniel <daniel.ruvcheski@students.finki.ukim.mk>
2025-03-28 22:04:51 +0100

    Enhance CreateJobForm by adding job title and employment type fields with validation. Integrate FormField, Select, and Input components for improved user experience.

--- components/forms/CreateJobForm.tsx
+++ components/forms/CreateJobForm.tsx
@@ -1,10 +1,13 @@
 "use client"
 import { useForm } from "react-hook-form";
-import { Form } from "../ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { jobSchema } from "@/app/utils/zodSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Input } from "../ui/input";
+
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select";
 
 
 
@@ -40,7 +43,48 @@ export function CreateJobForm() {
                         </CardTitle>
                     </CardHeader>
                     <CardContent>
-                        
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                            <FormField
+                             control={form.control} 
+                             name="jobTitle"
+                             render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Job Title</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Job Title" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>  
+                             )}
+                            />
+
+                            <FormField
+                             control={form.control} 
+                             name="employmentType"
+                             render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Employment Type</FormLabel>
+                                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                        <FormControl>
+                                            <SelectTrigger>
+                                                <SelectValue placeholder="Select Employment Type" />
+                                            </SelectTrigger>
+                                        </FormControl>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                <SelectLabel>Employment Type</SelectLabel>
+                                                <SelectItem value="full-time">Full-Time</SelectItem>
+                                                <SelectItem value="part-time">Part-Time</SelectItem>
+                                                <SelectItem value="internship">Internship</SelectItem>
+                                                <SelectItem value="contractor">Contractor</SelectItem>
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+                                    <FormMessage />
+                                </FormItem>  
+                             )}
+                            />
+                        </div>
                     </CardContent>
                 </Card>
             </form>
